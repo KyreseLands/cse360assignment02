bee2d0950f7a0f4b409c60ee2255798a879b01c4
Kyrese Lands <kdlands@asu.edu>
2020-10-07 00:39:44 -0700

    Initial checking of adding machine

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,42 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+ 
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /**
+   * stores inital total value of 0
+   */
+  public int getTotal () {
+    return 0;
+  }
+  /**
+   * Stores total value to be added or subtracted
+   */
+  public void add (int value) {
+  }
+/**
+ * Adds parameter value to total
+ * @param value
+ */
+  public void subtract (int value) {
+  }
+/**
+ * subtracts parameter value from total
+ */
+  public String toString () {
+    return "";
+  }
+/**
+ * Prints values that have been added
+ * or subtracted
+ */
+  public void clear() {
+  }
+  /**
+   * Clears the adding machine
+   * resets total to 0
+   */
+}
